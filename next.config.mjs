838dcac89aec31a5128008147a2e538d910cf27a
zehan12 <zehan9211@gmail.com>
2024-07-29 17:52:38 +0530

    upgrade to next 15

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    bundlePagesRouterDependencies: true,
+    experimental: {
+        reactCompiler: true,
+        turbo: {
+            loaders: {
+                // Add custom loaders here
+            },
+        },
+    },
+};
 
 export default nextConfig;
